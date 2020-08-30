e6242e25b116599b787679f897df0e712c212a73
Cooper Saye <coopersaye@gmail.com>
2020-08-30 12:24:32 -0700

    Set up program structure.

--- StringToBrainfuck.cpp
+++ StringToBrainfuck.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+std::string getUserString()
+{
+    std::string input;
+    std::cout << "Please enter a string to convert: ";
+    std::cin >> input;
+    return input;
+}
+
+std::string convertUserString(std::string input)
+{
+    return NULL;
+}
+
 int main()
 {
-    std::cout << "sup\n";
+    std::string input = getUserString();
+    std::string output = convertUserString(input);
+    std::cout << input << " in Brainfuck is " << output;
 }
