87d191e187b33b465ab40f4439ab11e0a1ac7ebe
Cooper Saye <coopersaye@gmail.com>
2020-08-29 20:59:55 -0700

    Implement classes and constructors.

--- CPPTesting.cpp
+++ CPPTesting.cpp
@@ -2,9 +2,55 @@
 
 using namespace std;
 
+class Person
+{
+    public:
+        string name;
+        int age;
+    
+    Person(string _name, int _age)
+    {
+        name = _name;
+        age = _age;
+    }
+};
+
+class Student
+{
+    public:
+        string name;
+        int age;
+        char grade;
+    
+    Student(string _name, int _age, char _grade)
+    {
+        name = _name;
+        age = _age;
+        grade = _grade;
+    }
+};
+
 void printInstructions()
 {
-    cout << "Welcome. Please enter people into the database with their name, age, and occupation." << endl;
+    cout << "Welcome to the student database.";
+}
+
+void addPerson()
+{
+    string name;
+    cout << "Please enter a name: ";
+    cin >> name;
+
+    int age;
+    cout << "Please enter an age: ";
+    cin >> age;
+
+    char grade;
+    cout << "Please enter a grade: ";
+    cin >> grade;
+
+
+    Student student(name, age, grade);
 }
 
 void getUserCommands()
@@ -19,6 +65,10 @@ void getUserCommands()
         if (input == "quit")
         {
             running = false;
+        }
+        else if (input == "add")
+        {
+            addPerson();
         }
     }
 }
