3fb1cfd37937473876535ec4efd74c4777f63fb3
Cooper Saye <coopersaye@gmail.com>
2020-08-31 23:02:59 -0700

    Set up Random program framework.

---
+++ Random.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+std::string promptUser()
+{
+    return NULL;
+}
+
+int generateRandom(std::string randomString)
+{
+    return 0;
+}
+
+void printRandomNumber(int randomNumber)
+{
+
+}
+
+int main()
+{
+    std::string randomString = promptUser();
+    int randomNumber = generateRandom(randomString);
+    printRandomNumber(randomNumber);
+}
