1d10ddde4b6d908c5d03c83f031180795e35505c
Cooper Saye <coopersaye@gmail.com>
2020-08-31 23:13:51 -0700

    Finish pseudo-random algorithm.

--- Random.cpp
+++ Random.cpp
@@ -2,17 +2,36 @@
 
 std::string promptUser()
 {
-    return NULL;
+    std::cout << "Please enter a random string: ";
+    std::string input;
+    std::cin >> input;
+    return input;
 }
 
 int generateRandom(std::string randomString)
 {
-    return 0;
+    int randomNumber = 1;
+
+    for (int i = 0; i < 8; i++)
+    {
+        if (randomString.length() - 1 < i) break;
+
+        if (i % 2 == 0)
+        {
+            randomNumber *= randomString[i];
+        }
+        else
+        {
+            randomNumber += randomString[i];
+        }
+    }
+
+    return randomNumber % 100;
 }
 
 void printRandomNumber(int randomNumber)
 {
-
+    std::cout << "The random number between 0 and 99 is: \n" << randomNumber << "\n";
 }
 
 int main()
